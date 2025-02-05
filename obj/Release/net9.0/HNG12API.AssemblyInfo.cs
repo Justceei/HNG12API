b1738987bba94de7bcb9852345421ea066fac351
Justceei <ekechukwu65@gmail.com>
2025-02-06 00:51:32 +0100

    Created a published version of the application

--- obj/Release/net9.0/HNG12API.AssemblyInfo.cs
+++ obj/Release/net9.0/HNG12API.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNG12API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea111a1b51e0190ebe479bb2542913780e38d3a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNG12API")]
